eb6e016baeb48274d2428fb98d0f7cb91542d922
Aarya Chitnis <aarya.chitnis28@gmail.com>
2023-01-29 22:36:31 +0000

    saving progress

--- client/src/LandingDisplay.js
+++ client/src/LandingDisplay.js
@@ -7,12 +7,12 @@ import {useNavigate} from "react-router-dom"
 const LandingDisplay = () => {
     const navigate = useNavigate();
     return (
-        <body>
+        <div>
             <h1 className={styles.headings}>Skill Scanner</h1>
             <h2 className={styles.headings}>Connecting clients with professionals</h2>
             <button className={styles.LoginBtnPos} onClick={()=>navigate("/login")}>Login</button>
             <button className={styles.RegisterBtnPos} onClick={()=>navigate("/register")}>Register</button>
-        </body>
+        </div>
     )
 };
   
